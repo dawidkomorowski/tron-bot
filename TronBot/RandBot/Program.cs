44cb241ae76c5e27903bc7dfb30298bdc754fe72
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-03-23 23:30:15 +0100

    Implemented rand bot.

---
+++ TronBot/RandBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using TronBotFramework;
+using TronBotFramework.Interface.Version1;
+
+namespace RandBot
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var runner = new TronBotRunner(new TronBot(), new TronBotInterface());
+            runner.Run();
+        }
+    }
+}
