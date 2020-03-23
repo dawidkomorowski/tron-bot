8e7f5b10d519420c1716086781cadf9b757ff116
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-03-23 01:29:36 +0100

    Implemented TronString class which represents type-safe, parsed tron string notation. It allows to create Board instance with stated specified by tron string.

---
+++ TronBot/TronBotFramework.UnitTests/Interface/Version1/TronStringTests.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+using TronBotFramework.Interface.Version1;
+
+namespace TronBotFramework.UnitTests.Interface.Version1
+{
+    [TestFixture]
+    public class TronStringTests
+    {
+        [TestCase("oooooX/orrr1o/o2R1o/o1Bbbo/o3bo/oooooo", "Unrecognized symbol 'X' at position 5.")]
+        [TestCase("oooooo/orrr1o/o2R1oo/o1Bbbo/o3bo/oooooo", "Number of fields for row 2 exceeded. Field over limit defined by symbol at 19.")]
+        [TestCase("oooooo/orrr1o/o2R1o/o12Bbbo/o3bo/oooooo", "Number of fields for row 3 exceeded. Empty fields over limit defined by symbol at 21.")]
+        [TestCase("oooooo/orrr1o/o2R1/o1Bbbo/o3bo/oooooo", "There are missing fields for row 2. Row unexpected ending defined by symbol at 18.")]
+        [TestCase("oooooo/orrr1o/o2R1o/o1Bbbo/o3bo/ooooo", "There are missing fields for row 5. Tron string unexpectedly ended.")]
+        [TestCase("oooooo/orrr1o/o2R1o/o1Bbbo/o3bo/", "There are missing fields for row 5. Tron string unexpectedly ended.")]
+        public void Constructor_ShouldThrowExceptionGivenInvalidTronString(string rawTronString, string expectedMessage)
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.That(() => new TronString(rawTronString), Throws.TypeOf<TronStringParsingException>().With.Message.Contains(expectedMessage));
+        }
+
+        [Test]
+        public void Create_ShouldCreateBoardBasedOnTronString()
+        {
+            // Arrange
+            const string rawTronString = "oooooo/orrr1o/o2R1o/o1Bbbo/o3bo/oooooo";
+
+            // Act
+            var parsedTronString = new TronString(rawTronString);
+
+            // Assert
+            var board = parsedTronString.Create();
+            Assert.That(board.Width, Is.EqualTo(6));
+            Assert.That(board.Height, Is.EqualTo(6));
+
+            Assert.That(board.GetField(0, 0), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(1, 0), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(2, 0), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(3, 0), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(4, 0), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(5, 0), Is.EqualTo(Field.Obstacle));
+
+            Assert.That(board.GetField(0, 1), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(1, 1), Is.EqualTo(Field.RedTail));
+            Assert.That(board.GetField(2, 1), Is.EqualTo(Field.RedTail));
+            Assert.That(board.GetField(3, 1), Is.EqualTo(Field.RedTail));
+            Assert.That(board.GetField(4, 1), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(5, 1), Is.EqualTo(Field.Obstacle));
+
+            Assert.That(board.GetField(0, 2), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(1, 2), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(2, 2), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(3, 2), Is.EqualTo(Field.RedHead));
+            Assert.That(board.GetField(4, 2), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(5, 2), Is.EqualTo(Field.Obstacle));
+
+            Assert.That(board.GetField(0, 3), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(1, 3), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(2, 3), Is.EqualTo(Field.BlueHead));
+            Assert.That(board.GetField(3, 3), Is.EqualTo(Field.BlueTail));
+            Assert.That(board.GetField(4, 3), Is.EqualTo(Field.BlueTail));
+            Assert.That(board.GetField(5, 3), Is.EqualTo(Field.Obstacle));
+
+            Assert.That(board.GetField(0, 4), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(1, 4), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(2, 4), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(3, 4), Is.EqualTo(Field.Empty));
+            Assert.That(board.GetField(4, 4), Is.EqualTo(Field.BlueTail));
+            Assert.That(board.GetField(5, 4), Is.EqualTo(Field.Obstacle));
+
+            Assert.That(board.GetField(0, 5), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(1, 5), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(2, 5), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(3, 5), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(4, 5), Is.EqualTo(Field.Obstacle));
+            Assert.That(board.GetField(5, 5), Is.EqualTo(Field.Obstacle));
+        }
+
+        [Test]
+        public void Create_ShouldCreateBoard_GivenTronStringWithMultiDigitNumbers()
+        {
+            // Arrange
+            const string rawTronString = "o27o/o26oo";
+
+            // Act
+            var parsedTronString = new TronString(rawTronString);
+
+            // Assert
+            var board = parsedTronString.Create();
+            Assert.That(board.Width, Is.EqualTo(29));
+            Assert.That(board.Height, Is.EqualTo(2));
+        }
+    }
+}
---
+++ TronBot/TronBotFramework/Interface/Version1/TronString.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TronBotFramework.Interface.Version1
+{
+    public sealed class TronString
+    {
+        private readonly Board _board;
+
+        public TronString(string tronString)
+        {
+            var tokens = ParseTokens(tronString);
+            _board = ParseDimensions(tokens);
+            ParseState(_board, tokens, tronString);
+        }
+
+        public Board Create()
+        {
+            return _board.Clone();
+        }
+
+        private static IReadOnlyCollection<Token> ParseTokens(string tronString)
+        {
+            var tokens = new List<Token>();
+            var position = 0;
+
+            while (position < tronString.Length)
+            {
+                if (TryParseSymbol(tronString, position, out var token))
+                {
+                    tokens.Add(token);
+                    position++;
+                }
+                else if (TryParseNumber(tronString, position, out var positionIncrement, out token))
+                {
+                    tokens.Add(token);
+                    position += positionIncrement;
+                }
+                else if (TryParseRowSeparator(tronString, position, out token))
+                {
+                    tokens.Add(token);
+                    position++;
+                }
+                else
+                {
+                    var symbol = tronString[position];
+                    throw new TronStringParsingException(tronString, $"Unrecognized symbol '{symbol}' at position {position}.");
+                }
+            }
+
+            return tokens.AsReadOnly();
+        }
+
+        private static bool TryParseSymbol(string tronString, int position, out Token token)
+        {
+            token = default;
+
+            var symbol = tronString[position];
+            switch (symbol)
+            {
+                case 'o':
+                    token = new Token(TokenType.Symbol, position, Field.Obstacle, default);
+                    return true;
+                case 'b':
+                    token = new Token(TokenType.Symbol, position, Field.BlueTail, default);
+                    return true;
+                case 'B':
+                    token = new Token(TokenType.Symbol, position, Field.BlueHead, default);
+                    return true;
+                case 'r':
+                    token = new Token(TokenType.Symbol, position, Field.RedTail, default);
+                    return true;
+                case 'R':
+                    token = new Token(TokenType.Symbol, position, Field.RedHead, default);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private static bool TryParseNumber(string tronString, int position, out int positionIncrement, out Token token)
+        {
+            token = default;
+            positionIncrement = default;
+
+            var stringBuilder = new StringBuilder();
+            var localPosition = position;
+            while (localPosition < tronString.Length && char.IsDigit(tronString[localPosition]))
+            {
+                stringBuilder.Append(tronString[localPosition]);
+                localPosition++;
+            }
+
+            if (stringBuilder.Length > 0)
+            {
+                positionIncrement = stringBuilder.Length;
+                var number = int.Parse(stringBuilder.ToString());
+                token = new Token(TokenType.Number, position, default, number);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool TryParseRowSeparator(string tronString, int position, out Token token)
+        {
+            token = default;
+
+            if (tronString[position] == '/')
+            {
+                token = new Token(TokenType.RowSeparator, position, default, default);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static Board ParseDimensions(IReadOnlyCollection<Token> tokens)
+        {
+            var width = tokens.TakeWhile(t => t.Type != TokenType.RowSeparator)
+                .Select(t => t.Type switch
+                    {
+                        TokenType.Symbol => 1,
+                        TokenType.Number => t.Number,
+                        _ => 0
+                    }
+                ).Sum();
+            var height = tokens.Count(t => t.Type == TokenType.RowSeparator) + 1;
+
+            return new Board(width, height);
+        }
+
+        private static void ParseState(Board board, IReadOnlyCollection<Token> tokens, string tronString)
+        {
+            var x = 0;
+            var y = 0;
+
+            foreach (var token in tokens)
+            {
+                switch (token.Type)
+                {
+                    case TokenType.Symbol:
+                        if (x >= board.Width)
+                        {
+                            throw new TronStringParsingException(tronString,
+                                $"Number of fields for row {y} exceeded. Field over limit defined by symbol at {token.Position}.");
+                        }
+
+                        board.SetField(x, y, token.Symbol);
+                        x++;
+                        break;
+                    case TokenType.Number:
+                        if (x + token.Number - 1 >= board.Width)
+                        {
+                            throw new TronStringParsingException(tronString,
+                                $"Number of fields for row {y} exceeded. Empty fields over limit defined by symbol at {token.Position}.");
+                        }
+
+                        for (var i = 0; i < token.Number; i++)
+                        {
+                            board.SetField(x, y, Field.Empty);
+                            x++;
+                        }
+
+                        break;
+                    case TokenType.RowSeparator:
+                        if (x != board.Width)
+                        {
+                            throw new TronStringParsingException(tronString,
+                                $"There are missing fields for row {y}. Row unexpected ending defined by symbol at {token.Position}.");
+                        }
+
+                        x = 0;
+                        y++;
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            if (x != board.Width)
+            {
+                throw new TronStringParsingException(tronString,
+                    $"There are missing fields for row {y}. Tron string unexpectedly ended.");
+            }
+        }
+
+        private enum TokenType
+        {
+            Symbol,
+            Number,
+            RowSeparator
+        }
+
+        private struct Token
+        {
+            public Token(TokenType type, int position, Field symbol, int number)
+            {
+                Type = type;
+                Position = position;
+                Symbol = symbol;
+                Number = number;
+            }
+
+            public TokenType Type { get; }
+            public int Position { get; }
+            public Field Symbol { get; }
+            public int Number { get; }
+
+            public override string ToString()
+            {
+                return $"{nameof(Type)}: {Type}, {nameof(Position)}: {Position}, {nameof(Symbol)}: {Symbol}, {nameof(Number)}: {Number}";
+            }
+        }
+    }
+
+    public sealed class TronStringParsingException : Exception
+    {
+        public TronStringParsingException(string rawTronString, string reason) : base($"Cannot parse following tron string: {rawTronString}. {reason}")
+        {
+            RawTronString = rawTronString;
+        }
+
+        public string RawTronString { get; }
+    }
+}
