9ae05461ce0d40c409a7da882d2a1966acc6e91c
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-03-23 23:29:31 +0100

    Implemented tron bot interface version 1.

---
+++ TronBot/TronBotFramework.UnitTests/Interface/Version1/TronBotInterfaceTests.cs
@@ -0,0 +1,109 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using TronBotFramework.Interface.Version1;
+
+namespace TronBotFramework.UnitTests.Interface.Version1
+{
+    [TestFixture]
+    public class TronBotInterfaceTests
+    {
+        private IBot _bot;
+        private IInterface _interface;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            _bot = Substitute.For<IBot>();
+            _interface = new TronBotInterface();
+        }
+
+        [Test]
+        public void InterfaceThrowsException_WhenFirstCommandIsNot_Tbi()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.That(() => _interface.ProcessCommand("not_tbi", _bot), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void InterfaceThrowsException_WhenSecondCommandIsNot_TbiV1()
+        {
+            // Arrange
+            _interface.ProcessCommand("tbi", _bot);
+
+            // Act
+            // Assert
+            Assert.That(() => _interface.ProcessCommand("not_tbi_v1", _bot), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void InterfaceThrowsException_WhenThirdCommandIsNeither_ColorBlue_Nor_ColorRed()
+        {
+            // Arrange
+            _interface.ProcessCommand("tbi", _bot);
+            _interface.ProcessCommand("tbi v1", _bot);
+
+            // Act
+            // Assert
+            Assert.That(() => _interface.ProcessCommand("not_color_blue", _bot), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void InterfaceInitializationHappyPath()
+        {
+            // Act
+            // Assert
+            var response = _interface.ProcessCommand("tbi", _bot);
+            Assert.That(response.Content, Is.EqualTo("tbi ok"));
+            Assert.That(response.WaitForNextCommand, Is.True);
+
+            response = _interface.ProcessCommand("tbi v1", _bot);
+            Assert.That(response.Content, Is.EqualTo("tbi v1 ok"));
+            Assert.That(response.WaitForNextCommand, Is.True);
+
+            response = _interface.ProcessCommand("color blue", _bot);
+            Assert.That(response.Content, Is.EqualTo("color ok"));
+            Assert.That(response.WaitForNextCommand, Is.True);
+        }
+
+        [Test]
+        public void InterfaceSupportsExitCommand()
+        {
+            // Arrange
+            _interface.ProcessCommand("tbi", _bot);
+            _interface.ProcessCommand("tbi v1", _bot);
+            _interface.ProcessCommand("color blue", _bot);
+
+            // Act
+            var response = _interface.ProcessCommand("exit", _bot);
+
+            // Assert
+            Assert.That(response.Content, Is.Empty);
+            Assert.That(response.WaitForNextCommand, Is.False);
+        }
+
+        [TestCase(Move.Up, "up", "color blue", Color.Blue)]
+        [TestCase(Move.Down, "down", "color red", Color.Red)]
+        [TestCase(Move.Left, "left", "color blue", Color.Blue)]
+        [TestCase(Move.Right, "right", "color red", Color.Red)]
+        public void InterfaceSupportsMoveCommand(Move botMove, string responseContent, string colorCommand, Color botColor)
+        {
+            // Arrange
+            _bot.FindMove(Arg.Any<Board>(), Arg.Any<Color>()).Returns(botMove);
+
+            _interface.ProcessCommand("tbi", _bot);
+            _interface.ProcessCommand("tbi v1", _bot);
+            _interface.ProcessCommand(colorCommand, _bot);
+
+            // Act
+            var response = _interface.ProcessCommand("move oooooooooooo/oB9o/o10o/o10o/o10o/o10o/o10o/o10o/o10o/o10o/o9Ro/oooooooooooo", _bot);
+
+            // Assert
+            Assert.That(response.Content, Is.EqualTo(responseContent));
+            Assert.That(response.WaitForNextCommand, Is.True);
+            _bot.Received(1).FindMove(Arg.Is<Board>(b => b != null && b.Width == 12 && b.Height == 12), botColor);
+        }
+    }
+}
---
+++ TronBot/TronBotFramework/Interface/Version1/TronBotInterface.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace TronBotFramework.Interface.Version1
+{
+    public sealed class TronBotInterface : IInterface
+    {
+        private State _currentState = State.WaitingForFirstCommand;
+        private Color _color;
+
+        public Response ProcessCommand(string command, IBot bot)
+        {
+            return _currentState switch
+            {
+                State.WaitingForFirstCommand => HandleCommandInState_WaitingForFirstCommand(command),
+                State.TronBotInterfaceAccepted => HandleCommandInState_TronBotInterfaceAccepted(command),
+                State.InterfaceVersionAccepted => HandleCommandInState_InterfaceVersionAccepted(command),
+                State.Ready => HandleCommandInState_Ready(command, bot),
+                _ => throw new ArgumentOutOfRangeException(nameof(_currentState), _currentState, "Unhandled state!")
+            };
+        }
+
+        private enum State
+        {
+            WaitingForFirstCommand,
+            TronBotInterfaceAccepted,
+            InterfaceVersionAccepted,
+            Ready
+        }
+
+        private Response HandleCommandInState_WaitingForFirstCommand(string command)
+        {
+            if (command != "tbi") throw new ArgumentException($"Expected 'tbi' command, received '{command}' command.", nameof(command));
+
+            _currentState = State.TronBotInterfaceAccepted;
+            return new Response("tbi ok", true);
+        }
+
+        private Response HandleCommandInState_TronBotInterfaceAccepted(string command)
+        {
+            if (command != "tbi v1") throw new ArgumentException($"Expected 'tbi v1' command, received '{command}' command.", nameof(command));
+
+            _currentState = State.InterfaceVersionAccepted;
+            return new Response("tbi v1 ok", true);
+        }
+
+        private Response HandleCommandInState_InterfaceVersionAccepted(string command)
+        {
+            if (command != "color blue" && command != "color red")
+                throw new ArgumentException($"Expected either 'color blue' command or 'color red' command, received '{command}' command.", nameof(command));
+
+            var colorString = ExtractColorString(command);
+            _color = ParseColor(colorString);
+            _currentState = State.Ready;
+            return new Response("color ok", true);
+        }
+
+        private Response HandleCommandInState_Ready(string command, IBot bot)
+        {
+            if (command == "exit")
+            {
+                return new Response(string.Empty, false);
+            }
+            else if (IsMoveCommand(command))
+            {
+                var rawTronString = ExtractTronString(command);
+                var tronString = new TronString(rawTronString);
+                var move = bot.FindMove(tronString.Create(), _color);
+                return new Response(MapMoveResponse(move), true);
+            }
+            else
+            {
+                throw new ArgumentException($"Unrecognized command: {command}.", nameof(command));
+            }
+        }
+
+        private static bool IsMoveCommand(string command)
+        {
+            return command.StartsWith("move");
+        }
+
+        private static string ExtractTronString(string moveCommand)
+        {
+            return new string(moveCommand.Skip(5).ToArray());
+        }
+
+        private static string MapMoveResponse(Move move) => move switch
+        {
+            Move.Up => "up",
+            Move.Down => "down",
+            Move.Left => "left",
+            Move.Right => "right",
+            _ => throw new ArgumentOutOfRangeException(nameof(move), move, "Unhandled move!")
+        };
+
+        private static string ExtractColorString(string colorCommand)
+        {
+            return new string(colorCommand.Skip(6).ToArray());
+        }
+
+        private static Color ParseColor(string color) => color switch
+        {
+            "blue" => Color.Blue,
+            "red" => Color.Red,
+            _ => throw new ArgumentOutOfRangeException(nameof(color), color, "Unrecognized color!")
+        };
+    }
+}
